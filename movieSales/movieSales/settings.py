1587326cf41832bae26165062e515182dc2d9902
Yoeli Díaz <ada_hack183@hackademy.mx>
2023-06-17 15:02:05 -0600

    Modificaciones

--- movieSales/movieSales/settings.py
+++ movieSales/movieSales/settings.py
@@ -60,7 +60,8 @@ INSTALLED_APPS += [
     'users',
     'address',
     'movies',
-    'stores'
+    'stores',
+    'cards'
 ]
 
 MIDDLEWARE = [
--- movieSales/stores/models.py
+++ movieSales/stores/models.py
@@ -4,9 +4,17 @@ from movies.models import MovieModel
 
 # Create your models here.
 class StoreModel(models.Model):
+    SUCURSALES =[
+        ('INSURGENTES', 'insurgentes'),
+        ('CUATRO CAMINOS', 'cuatro caminos'),
+        ('LOMAS ESTRELLA', 'lomas estrella'),
+        ('SANTA FE', 'santa fe'),
+        ('UNIVERSIDAD', 'universidad')
+    ]
+
     user_id = models.ManyToManyField(UserModel)
     movies_id = models.ManyToManyField(MovieModel)
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(choices=SUCURSALES, unique=True)
     location = models.CharField(max_length=50, unique=True)
     store_hours = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)#auto_now_add se genera cuando se invoca, auto_now cuando se crea
